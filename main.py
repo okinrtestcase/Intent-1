f586b53c8575c94052bf986fa316e85f1d99315e
okinr.testcase <okinr.testcase@gmail.com>
2026-01-20 09:12:16 +0700

    Test Bank Jakarta - Part 06

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     id_test = modul.id_test()
 
     # report name
-    report_filename = "Test Intent Bank Jakarta - Part 05"
+    report_filename = "Test Intent Bank Jakarta - Part 06"
     # Logging
     modul.setup_logging(report_filename, id_test)
 
@@ -42,8 +42,8 @@ def main():
     url = "https://chat.botika.online/TF00WgP" # Bank DKI Dev
 
     """ Filename Assets """
-    csv_file  = "bankjakarta-05"
-    json_file = "bankjakarta-05"
+    csv_file  = "bankjakarta-06"
+    json_file = "bankjakarta-06"
     
 
     """ Detail of test """
